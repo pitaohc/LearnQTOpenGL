060f799e8ef0fc59301eb08937ca25d91c677330
pitao <pitaohc@qq.com>
2023-09-21 18:50:41 +0800

    使用dlDrawElements绘制三角形

--- src/openglwidget.cpp
+++ src/openglwidget.cpp
@@ -1,5 +1,5 @@
 #include "openglwidget.h"
-
+#include <qdebug.h>
 
 
 OpenGLWidget::OpenGLWidget(QWidget* parent) : QOpenGLWidget(parent)
@@ -11,6 +11,67 @@ OpenGLWidget::OpenGLWidget(QWidget* parent) : QOpenGLWidget(parent)
 void OpenGLWidget::initializeGL()
 {
     initializeOpenGLFunctions();
+
+    // 设置渲染方式
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    int success;
+    char infoLog[512];
+    // 编译着色器
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader,1,&vertexShaderSource,nullptr);
+    glCompileShader(vertexShader);
+    // 检查编译是否成功
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader,512,nullptr,infoLog);
+        qDebug() << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog;
+    }
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        qDebug() << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog;
+    }
+
+    // 创建着色器程序
+    shaderProgram = glCreateProgram();
+    // 将着色器附加到程序上
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    // 链接着色器程序
+    glLinkProgram(shaderProgram);
+    // 检查链接是否成功
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram,512,nullptr,infoLog);
+        qDebug() << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog;
+    }
+    // 删除着色器
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    qDebug() << "Compile Shader success!!!\n";
+
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+
+    qDebug() << "Create VAO success!!!\n";
 }
 
 void OpenGLWidget::resizeGL(int w, int h)
@@ -21,4 +82,9 @@ void OpenGLWidget::paintGL()
 {
     glClearColor(0.5f, 0.6f, 0.7f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    //glDrawArrays(GL_TRIANGLES,0,3);
+    glDrawElements(GL_TRIANGLES,sizeof(indices), GL_UNSIGNED_INT, 0);
 }
--- src/openglwidget.h
+++ src/openglwidget.h
@@ -14,7 +14,31 @@ protected:
     void initializeGL() override;
     void resizeGL(int w, int h) override;
     void paintGL() override;
-
+private:
+    unsigned int VAO, VBO, EBO;
+    unsigned int shaderProgram;
+    float vertices[12] = {
+        +0.5f, +0.5f, +0.0f, 
+        -0.5f, +0.5f, +0.0f,
+        -0.5f, -0.5f, +0.0f,
+        +0.5f, -0.5f, +0.0f
+    };
+    unsigned int indices[6] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+    const char* vertexShaderSource = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+    const char* fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 FragColor;\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\n\0";
 signals:
 
 };
