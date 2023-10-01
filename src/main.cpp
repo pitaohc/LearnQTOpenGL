0a6b9bcc367c576bc58aac4173a486217e022791
pitao <pitaohc@qq.com>
2023-10-01 19:10:53 +0800

    添加openmp支持

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 
 #include <QApplication>
 #include <qdebug.h>
-
+#include <omp.h>
 int main(int argc, char* argv[])
 {
-
+    qDebug() << "number of available processors: " << omp_get_num_procs();
+    qDebug() << "number of threads: " << omp_get_max_threads();
     qDebug() << "Qt version:" << QT_VERSION_STR;
 
     
